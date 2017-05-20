727cdbb778686b3a3e60b9b46314c893716091ff
josephtyx <joseph.teng_2015@sit.singaporetech.edu.sg>
2017-05-20 19:40:16 +0800

    Register and Login file

---
+++ ClientFile/Login.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  	<link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+  	<!--<div class="container-fluid">
+  		<div>
+		  <h1 id="logo">Joel's Logo</h1>
+		  <p>This is some text.</p>
+		</div>
+	</div>-->
+	<div class="jumbotron text-center">
+	  <h1>Company Logo</h1>
+	</div>
+
+	<div class="container">
+	  <div class="row">
+	    <div class="col-xs-6 ">
+	      <h3>Username</h3>
+	    </div>
+	    <div class="col-xs-6 ">
+	    	<input type="text" class="form-control" id="usr">
+	    </div>
+	  </div>
+	  <div class="row">
+	  	<div class="col-xs-12">
+	  		<h3>Password</h3>
+	  	</div>
+	  </div>
+	</div>
+  </body>
+</html>
