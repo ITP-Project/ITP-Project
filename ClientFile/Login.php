0ac2351d0ee6ff1523320421ae0958b416c96526
josephtyx <joseph.teng_2015@sit.singaporetech.edu.sg>
2017-05-21 20:40:56 +0800

    login page

--- ClientFile/Login.php
+++ ClientFile/Login.php
@@ -3,62 +3,99 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
   	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   	<link rel="stylesheet" type="text/css" href="CSS/style.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700|Lato:400,100,300,700,900' rel='stylesheet' type='text/css'>-->
+
+	<!-- Google Fonts -->
+	<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700|Lato:400,100,300,700,900' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="CSS/style.css">
+
+	<title>Volunteer Scheduling Application</title>
   </head>
   <body>
-  	<!--<div class="container-fluid">
-  		<div>
-		  <h1 id="logo">Joel's Logo</h1>
-		  <p>This is some text.</p>
-		</div>
-	</div>-->
-	<div class="jumbotron text-center">
-	  <h1>Company Logo</h1>
-	</div>
-
-	<div class="container">
-	  <div class="row">
-	    <div class="col-xs-6 ">
-	      <h3>Username</h3>
-	    </div>
-	    <div class="col-xs-6 ">
-	    	<input type="text" class="form-control" id="usr">
-	    </div>
-	  </div>
-	  <div class="row">
-	  	<div class="col-xs-6">
-	  		<h3>Password</h3>
-	  	</div>
-	  	<div class="col-xs-6">
-	  		<input type="text" class="form-control" id="pwd">
-	  	</div>
-	  </div>
-	  <!--Modal-->
-	  <div class="row">
-	  	<a href="#openModal">Forgot Password?</a>
-	  	<div id="openModal" class="modalDialog">
-			<div>
-				<a href="#close" title="Close" class="close">X</a>
-				<h2>Forgot your password?</h2>
-				<p>Enter Email</p>
-				<input type="text" class="form-control" id="email"><br>
-				<button type="button" class="btn">Submit
+  	<div class="container">
+  		<img src="" >
+  		<p style="text-align:center">Image here</p>
+  	</div>
+	<div class="container" style="text-align:center">
+		<div class="login-box animated fadeInUp">
+			<div class="box-header">
+				<h2>Log In</h2>
 			</div>
+			<label for="username">Username</label>
+			<br/>
+			<input type="text" id="username">
+			<br/>
+			<label for="password">Password</label>
+			<br/>
+			<input type="password" id="password">
+			<br/>
+			<button type="submit">Sign In</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			<button type="button" data-toggle="modal" data-target="#myModalReg">Sign Up</button>
+			<br/>
+			<a href="#" data-toggle="modal" data-target="#myModal"><p class="small">Forgot your password?</p></a>
 		</div>
+	</div>
+	<!-- Modal for password-->
+	<div id="myModal" class="modal fade" role="dialog">
+	  <div class="modal-dialog">
+
+	    <!-- Modal content-->
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <button type="button" class="close" data-dismiss="modal">&times;</button>
+	        <h4 class="modal-title">Forgot your password?</h4>
+	      </div>
+	      <div class="modal-body">
+	      	<table>
+	      		<tr>
+	      			<td>Enter Email</td>
+	      			<td>&nbsp;</td>
+	      			<td>&nbsp;</td>
+	      			<td>&nbsp;</td>
+	      			<td><input type="text" id="username"></td>
+	      		</tr>
+	      	</table>
+	      	<button type="button" class="btn btn-primary">Send</button>
+	      </div>
+	    </div>
+
 	  </div>
-	  <!--Login Button-->
-	  <div class="row">
-	  	<div class="col-xs-6">
-	  		<button type="button" class="btn">Login
-	  	</div>
-	  	<div class="col-xs-6">
-	  		<button type="button" class="btn">SignUp
-	  	</div>
+
+	  <!-- Modal for registeration -->
+	  <div id="myModalReg" class="modal fade" role="dialog">
+	  <div class="modal-dialog">
+
+	    <!-- Modal content-->
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <button type="button" class="close" data-dismiss="modal">&times;</button>
+	        <h4 class="modal-title">Forgot your password?</h4>
+	      </div>
+	      <div class="modal-body">
+	      	<table>
+	      		<tr>
+	      			<td>Enter Email</td>
+	      			<td>&nbsp;</td>
+	      			<td>&nbsp;</td>
+	      			<td>&nbsp;</td>
+	      			<td><input type="text" id="username"></td>
+	      		</tr>
+	      	</table>
+	      	<button type="button" class="btn btn-primary">Send</button>
+	      </div>
+	    </div>
+
 	  </div>
+
 	</div>
   </body>
 </html>
