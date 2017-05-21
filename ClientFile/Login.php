aa9badb490895b27fdea5e793d5a2e8b8d65773d
josephtyx <joseph.teng_2015@sit.singaporetech.edu.sg>
2017-05-21 17:30:24 +0800

    Updated code

--- ClientFile/Login.php
+++ ClientFile/Login.php
@@ -7,7 +7,7 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
   	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  	<link rel="stylesheet" type="text/css" href="style.css">
+  	<link rel="stylesheet" type="text/css" href="CSS/style.css">
   </head>
   <body>
   	<!--<div class="container-fluid">
@@ -30,8 +30,33 @@
 	    </div>
 	  </div>
 	  <div class="row">
-	  	<div class="col-xs-12">
+	  	<div class="col-xs-6">
 	  		<h3>Password</h3>
+	  	</div>
+	  	<div class="col-xs-6">
+	  		<input type="text" class="form-control" id="pwd">
+	  	</div>
+	  </div>
+	  <!--Modal-->
+	  <div class="row">
+	  	<a href="#openModal">Forgot Password?</a>
+	  	<div id="openModal" class="modalDialog">
+			<div>
+				<a href="#close" title="Close" class="close">X</a>
+				<h2>Forgot your password?</h2>
+				<p>Enter Email</p>
+				<input type="text" class="form-control" id="email"><br>
+				<button type="button" class="btn">Submit
+			</div>
+		</div>
+	  </div>
+	  <!--Login Button-->
+	  <div class="row">
+	  	<div class="col-xs-6">
+	  		<button type="button" class="btn">Login
+	  	</div>
+	  	<div class="col-xs-6">
+	  		<button type="button" class="btn">SignUp
 	  	</div>
 	  </div>
 	</div>
